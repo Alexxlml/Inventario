f0e383cc66ed69b4e933394abfbecd865db4e85f
Marco Zacarias <alexx_lml@outlook.com>
2021-10-21 16:49:59 -0500

    Inserción en función registrar
    - Se agregó la inserción a la tabla productos correctamente junto a la redirección a la misma página

--- app/Http/Livewire/Registro.php
+++ app/Http/Livewire/Registro.php
@@ -5,13 +5,18 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Sucursal;
 use App\Models\Categoria;
+use App\Models\Producto;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\DB;
 
 class Registro extends Component
 {
-public $nombre, $descripcion,$categoria_seleccionada, $sucursal_seleccionada, $precio, $fecha_compra;
-public $categorias, $sucursales;
+    public $nombre, $descripcion, $categoria_seleccionada, $sucursal_seleccionada, $precio, $fecha_compra;
+    public $categorias, $sucursales;
 
-    public function mount(){
+    public function mount()
+    {
         $this->categorias = Categoria::all();
         $this->sucursales = Sucursal::all();
     }
@@ -21,7 +26,29 @@ public $categorias, $sucursales;
         return view('livewire.registro');
     }
 
-    public function registrar(){
-        return redirect()->route('registro');
+    public function registrar()
+    {
+
+        try {
+            DB::transaction(
+                function () {
+                    Producto::create([
+                        'nombre' => $this->nombre,
+                        'descripcion' => $this->descripcion,
+                        'categoria_id' => $this->categoria_seleccionada,
+                        'sucursal_id' => $this->sucursal_seleccionada,
+                        'estado_id' => 1,
+                        'precio' => $this->precio,
+                        'fecha_compra' => $this->fecha_compra,
+                        'fecha_modificacion' => Carbon::today()->isoFormat('YYYY-MM-DD'),
+                        'comentarios' => NULL,
+                    ]);
+                }
+            );
+
+            return redirect()->route('registro');
+        } catch (Exception $ex) {
+            dd($ex);
+        }
     }
 }
