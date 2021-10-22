5c3d3a6b156c09f4f24b198696071405ab2b7a44
Marco Zacarias <alexx_lml@outlook.com>
2021-10-21 19:00:41 -0500

    Llenado de UserSeeder
    - Se agregaron 3 usuarios cada uno con uno de los 3 perfiles disponibles a elegir

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,6 +14,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // ! Administrador
+        User::create([
+            'name' => 'Marco Zacarias',
+            'email' => 'mazacariasr@testing.com',
+            'password' => bcrypt('MaryTierra1234'),
+            'profile_id' => '1',
+        ]);
+
+        // ? Gestor
+        User::create([
+            'name' => 'José Mendoza',
+            'email' => 'jmendoza@testing.com',
+            'password' => bcrypt('MaryTierra1234'),
+            'profile_id' => '2',
+        ]);
+
+        // * Capturista
+        User::create([
+            'name' => 'Ernesto Rodriguez',
+            'email' => 'erodriguez@testing.com',
+            'password' => bcrypt('MaryTierra1234'),
+            'profile_id' => '3',
+        ]);
     }
 }
