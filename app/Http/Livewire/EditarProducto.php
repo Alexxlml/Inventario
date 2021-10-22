b9b706571619c6b761b3188b7f545ee2a889f573
Marco Zacarias <alexx_lml@outlook.com>
2021-10-21 22:36:20 -0500

    Vista de edición
    - Se recicló la vista de Registro para ser adaptada a la de edición.
    
    - Se recibió el id en la función mount y se consultó el producto a la base de datos

--- app/Http/Livewire/EditarProducto.php
+++ app/Http/Livewire/EditarProducto.php
@@ -3,9 +3,46 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Sucursal;
+use App\Models\Categoria;
+use App\Models\Producto;
 
 class EditarProducto extends Component
 {
+    public $nombre, $descripcion, $categoria_seleccionada, $sucursal_seleccionada, $precio, $fecha_compra;
+    public $categorias, $sucursales;
+    public $producto;
+
+    protected $rules = [
+        'nombre' => 'required|regex:/^([0-9a-zA-ZùÙüÜäàáëèéïìíöòóüùúÄÀÁËÈÉÏÌÍÖÒÓÜÚñÑ\.\,\-\s]+)$/|max:30',
+        'descripcion' => 'required|regex:/^([0-9a-zA-ZùÙüÜäàáëèéïìíöòóüùúÄÀÁËÈÉÏÌÍÖÒÓÜÚñÑ\.\,\-\s]+)$/|max:100',
+        'categoria_seleccionada' => 'required',
+        'sucursal_seleccionada' => 'required',
+        'precio' => 'required|regex:/^([1-9][0-9]{0,4})$/',
+        'fecha_compra' => 'required',
+    ];
+
+    protected $messages = [
+        'nombre.required' => 'Este campo no puede permanecer vacío',
+        'nombre.regex' => 'Solo se permiten letras, números, puntos, comas y giones medios',
+        'nombre.max' => 'Se permiten 30 caracteres como máximo',
+        'descripcion.required' => 'Este campo no puede permanecer vacío',
+        'descripcion.regex' => 'Solo se permiten letras, números, puntos, comas y giones medios',
+        'descripcion.max' => 'Se permiten 100 caracteres como máximo',
+        'categoria_seleccionada.required' => 'Este campo no puede permanecer vacío',
+        'sucursal_seleccionada.required' => 'Este campo no puede permanecer vacío',
+        'precio.required' => 'Este campo no puede permanecer vacío',
+        'precio.regex' => 'Solo se permiten 5 dígitos como máximo entre el 1 y el 99999',
+        'fecha_compra.required' => 'Este campo no puede permanecer vacío',
+    ];
+
+    public function mount($id)
+    {
+        $this->producto = Producto::find($id);
+        $this->categorias = Categoria::all();
+        $this->sucursales = Sucursal::all();
+    }
+    
     public function render()
     {
         return view('livewire.editar-producto');
