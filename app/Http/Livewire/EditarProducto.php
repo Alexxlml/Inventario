50f35ae24afba544969b5ae28619113b73a0f09b
Marco Zacarias <alexx_lml@outlook.com>
2021-10-21 21:57:14 -0500

    Creación de componente EditarProducto

---
+++ app/Http/Livewire/EditarProducto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EditarProducto extends Component
+{
+    public function render()
+    {
+        return view('livewire.editar-producto');
+    }
+}
