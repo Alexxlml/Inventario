fb233e52fa185f2e06ef9df7c6a5adfcd0faeec2
Marco Zacarias <alexx_lml@outlook.com>
2021-10-21 20:53:18 -0500

    Creación de tabla para modulo Bandeja
    - Se creó la vista con la tabla para el modulo Bandeja

--- app/Http/Livewire/Bandeja.php
+++ app/Http/Livewire/Bandeja.php
@@ -3,11 +3,31 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Producto;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 
 class Bandeja extends Component
 {
+    use WithPagination;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage'
+    ];
+
+    public $search, $perPage = '5';
+
     public function render()
     {
-        return view('livewire.bandeja');
+        return view('livewire.bandeja', [
+            'productos' => DB::table('productos')
+                ->join('categorias', 'productos.categoria_id', '=', 'categorias.id')
+                ->join('sucursales', 'productos.sucursal_id', '=', 'sucursales.id')
+                ->select('productos.id', 'productos.nombre', 'categorias.nombre_categoria', 'sucursales.nombre_sucursal')
+                ->where('nombre', 'LIKE', "%{$this->search}%")
+                ->orderBy('id', 'DESC')
+                ->paginate($this->perPage),
+        ]);
     }
 }
