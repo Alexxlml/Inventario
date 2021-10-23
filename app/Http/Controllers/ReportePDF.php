5295439eb6407ce3525023d8ec8510b568bbf650
Marco Zacarias <alexx_lml@outlook.com>
2021-10-23 01:02:59 -0500

    Plantilla y Controlador funcionando
    - El reporte se genera adecuadamente trayendo la información de la BD

--- app/Http/Controllers/ReportePDF.php
+++ app/Http/Controllers/ReportePDF.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use PDF;
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ReportePDF extends Controller
 {
-    //
+    public function createPDF(){
+
+        $datosProducto = DB::table('productos')
+        ->join('categorias', 'productos.categoria_id', '=', 'categorias.id')
+        ->join('sucursales', 'productos.sucursal_id', '=', 'sucursales.id')
+        ->join('estados', 'productos.estado_id', '=', 'estados.id')
+        ->select(
+            'productos.id',
+            'productos.nombre',
+            'productos.descripcion',
+            'categorias.nombre_categoria',
+            'sucursales.nombre_sucursal',
+            'estados.nombre_estado',
+            'productos.precio',
+            'productos.fecha_compra',
+            'productos.comentarios',
+            'productos.created_at',
+            'productos.updated_at'
+        )
+        ->where('productos.id', '=' ,2)
+        ->get();
+
+        $pdf = PDF::loadView(
+            'PDF/reporte-pdf',
+            compact(
+                "datosProducto",
+            )
+        );
+        return $pdf->stream('colaboradores.pdf');
+    }
 }
