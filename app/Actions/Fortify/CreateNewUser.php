28bf7c2d56db8e8834e5cd31003f1e40b8b964ca
Marco Zacarias <alexx_lml@outlook.com>
2021-10-23 16:40:20 -0500

    Campos nuevos en ActionsFortify
    - Se añadieron los nuevos campos a esta clase para validar y añadir a la inserción del usuario en la base de datos

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,6 +22,9 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'lastname2' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:12'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
@@ -29,6 +32,10 @@ class CreateNewUser implements CreatesNewUsers
 
         return User::create([
             'name' => $input['name'],
+            'lastname' => $input['lastname'],
+            'lastname2' => $input['lastname2'],
+            'access' => 1,
+            'username' => $input['username'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
             'profile_id' => $input['profile_id'],
