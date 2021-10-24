13e60f1a9329e897abd5c4deb148a61db5e3cda5
Marco Zacarias <alexx_lml@outlook.com>
2021-10-23 22:14:05 -0500

    Redirecciones en Dashboard
    - Se modificó el comportamiento del dashboard para mostrar los componentes principales a los que tiene acceso cada perfil en la página principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            @if (Auth()->user()->profile_id == 1)
+            {{ __('Registro') }}
+            @elseif (Auth()->user()->profile_id == 1)
+            {{ __('Bandeja') }}
+            @elseif (Auth()->user()->profile_id == 1)
+            {{ __('Registro') }}
+            @else
             {{ __('Dashboard') }}
+            @endif
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @if (Auth()->user()->profile_id == 1)
+                @livewire('registro')
+                @elseif (Auth()->user()->profile_id == 2)
+                @livewire('bandeja')
+                @elseif (Auth()->user()->profile_id == 3)
+                @livewire('registro')
+                @else
                 <x-jet-welcome />
+                @endif
             </div>
         </div>
     </div>
